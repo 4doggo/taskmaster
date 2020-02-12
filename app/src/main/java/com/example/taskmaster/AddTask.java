01fb0247d57dd06091542ceeeb75a23ff949b0f6
shingoed <nakajima.shingo@gmail.com>
2020-02-12 12:50:36 -0800

    buttons working

--- app/src/main/java/com/example/taskmaster/AddTask.java
+++ app/src/main/java/com/example/taskmaster/AddTask.java
@@ -2,7 +2,11 @@ package com.example.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class AddTask extends AppCompatActivity {
 
@@ -10,6 +14,8 @@ public class AddTask extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+
+
     }
 
 }
--- app/src/main/java/com/example/taskmaster/MainActivity.java
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -1,65 +1,51 @@
 package com.example.taskmaster;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    static String TAG = "example.main";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // actually set the contents to be whatever's in the XML file called activity_main
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        // setup work
+        // logging: verbose, debug, info, warning, error, wtf
+        Log.d(TAG, "we are in onCreate");
 
-       FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        final Button buttonAddTask = findViewById(R.id.addTask);
+        buttonAddTask.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+
+                startActivity(new Intent(MainActivity.this, AddTask.class));
             }
         });
 
 
-final Button addTaskButton = findViewById(R.id.button);
+        final Button buttonAllTask = findViewById(R.id.allTasksButton);
+        buttonAllTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG,"made it to onclick method");
 
-            Intent i = new Intent(this, AddTask.class);
-            startActivity(i);
+                startActivity(new Intent(MainActivity.this, AllTask.class));
+//                Intent goToAllTask = new Intent(MainActivity.this, AllTask.class);
+//                MainActivity.this.startActivity(goToAllTask);
+
+                Log.i(TAG,"inside start activity");
+            }
+        });
 
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-
-
 
 }
